a65c51ec60342cc1276e278c8b560924bb0349e5
Can Aker <can.aker0@gmail.com>
2026-07-07 22:07:36 +0300

    v1.7 after ServiceCta and _ServiceCta checkpoint

---
+++ Internship1/Models/ServiceCtaViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Internship1.Models
+{
+    public class ServiceCtaViewModel
+    {
+        public string Label { get; set; } = string.Empty;
+
+        public string Title { get; set; } = string.Empty;
+
+        public string HighlightedTitle { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string PrimaryButtonText { get; set; } = string.Empty;
+
+        public string SecondaryButtonText { get; set; } = string.Empty;
+    }
+}
